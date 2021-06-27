011ae8e4e9a70e5355f11985a6e0df598bcf8371
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-06-27 16:17:55 +0200

    created system test

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -4,7 +4,13 @@ require "application_system_test_case"
 class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url
-  
-    assert_selector "h1", text: "Product"
+    # save_and_open_screenshot
+    assert_selector "h1", text: "Awesome Products" # if it's true, the test PASSED
+  end
+
+  test "displys as much cards on Index page as products in db" do
+    visit root_url
+    # save_and_open_screenshot
+    assert_selector ".card-product", count: Product.count
   end
 end
